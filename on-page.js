32ee5dabb797d3786e4a762419153c9f189871fa
Mark Notton <mark@marknotton.uk>
2017-01-12 23:41:28 +0000

    Added second function for elements no on the page Added a range of fixes Added callback function to return the queried element object. This allows you to use ‘this’

--- on-page.js
+++ on-page.js
@@ -18,37 +18,67 @@
 
 // $('.sidebar').onPage(5000, someFunction);
 
-// TODO: Make if so you can pass params in the function
-
 (function($){
 
-    // Add the selector, function, and breakpoint rules into an array, to be called when the screen resizes
-    $.fn.onPage = function() {
-        var args = Array.prototype.slice.call(arguments);
-        var delay = false;
-        var callback = false;
+  $.fn.onPage = function() {
+    var args = Array.prototype.slice.call(arguments),
+        delay = false,
+        callbackTrue = null,
+        callbackFalse = null;
 
-        if(typeof args[0] == 'number') {
-            delay = args.shift();
-            callback = args.shift();
+    // Check if first argument is a number. If it is, define delay variables
+    // and remove it from the args array.
+    if(typeof args[0] == 'number') {
+      delay = args.shift();
+    }
+
+    // Loop through remaining arguments
+    for(var i = 0; i < args.length; ++ i) {
+      var setting = args[i];
+
+      // Check for functions
+      if (typeof setting == 'function') {
+        if ( callbackTrue == null ) {
+          callbackTrue = setting;
+        } else if ( callbackFalse == null ) {
+          callbackFalse = setting;
+        }
+      }
+    }
+
+    var selector = this;
+
+    if (delay !== false && callbackTrue == null && callbackFalse == null) {
+      console.warn('On Page: You have defined a ' + delay + ' second delay, but no functions to callback when the timeout is complete. Add atleast one callback function in the second argument', selector);
+    }
+
+    if ($(selector).length) {
+      // Dpes exist on page
+      if (callbackTrue != null) {
+        if ( delay && delay !== 0) {
+          setTimeout(function() {
+            callbackTrue.apply(selector);
+          }, delay*1000);
         } else {
-            callback = args.shift();
+          callbackTrue.apply(selector);
         }
-
-        if (typeof callback == 'function') {
-            // Perform a few checks to make sure the selector that is being checked is valid
-            var selector = this.selector !== undefined && this.selector !== null && $(this.selector).length ? this.selector : null;
-
-            if (selector) {
-                if ( delay && delay !== 0) {
-                    setTimeout(function() {
-                        callback.apply($(selector), args);
-                    }, delay);
-                } else {
-                    callback.apply($(selector), args);
-                }
-            }
+      } else {
+        return selector;
+      }
+    } else {
+      // Doesn't exist on page
+      if (callbackFalse != null) {
+        if ( delay && delay !== 0) {
+          setTimeout(function() {
+            callbackFalse.apply(selector);
+          }, delay*1000);
+        } else {
+          callbackFalse.apply(selector);
         }
-    };
+      } else {
+        return false;
+      }
+    }
+  };
 
 })( jQuery );
